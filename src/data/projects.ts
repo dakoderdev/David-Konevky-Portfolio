c342f4bdf4184fd73a65311bb42073b8721aca36
dakodev <123116563+dakoderdev@users.noreply.github.com>
2026-09-15 01:32:23 -0300

    add data for the work section

---
+++ src/data/projects.ts
@@ -0,0 +1,28 @@
+const PROJECTS = [
+    {
+        name: "Corralon Artesan",
+        stack: ["Next.js", "Tailwind CSS", "Vercel"],
+        image: "/projects/corralon-artesan.png",
+        link: "https://corralon-artesan.com/",
+    },
+    {
+        name: "Showboxd",
+        stack: ["Next.js", "Tailwind CSS", "Supabase", "Vercel"],
+        image: "/projects/showboxd.png",
+        link: "https://showboxd.vercel.app/",
+    },
+    {
+        name: "Vinimusic",
+        stack: ["Next.js", "Auth.js", "Tailwind CSS", "Zustand", "Netlify"],
+        image: "/projects/vinimusic.png",
+        link: "https://vinimusic.netlify.app/",
+    },
+    {
+        name: "Ticketz",
+        stack: ["JavaScript", "CSS", "HTML"],
+        image: "/projects/ticketz.png",
+        link: "https://ticketzok.netlify.app/",
+    }
+]
+
+export default PROJECTS;
